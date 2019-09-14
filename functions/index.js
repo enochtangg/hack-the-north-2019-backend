98851f69752cd5deba92a1af42ad04becdadc260
enochtangg <enoch.tang@hotmail.com>
2019-09-14 11:16:32 -0400

    first working cloud function with firestore write

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
