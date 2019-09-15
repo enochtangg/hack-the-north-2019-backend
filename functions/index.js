396fb55d6cb2cb814d9d3139a38bfb418ad8c4d2
enochtangg <enoch.tang@hotmail.com>
2019-09-14 21:41:15 -0400

    working contributing to pool and logging past transactions

--- functions/index.js
+++ functions/index.js
@@ -8,11 +8,77 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
+exports.pingFunction = functions.https.onRequest(async (req, res) => {
+    res.json({result: "Pong"});
+});
+
+exports.getCurrentPool = functions.https.onRequest(async (req, res) => {
     // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({original: original});
-    // Send back a message that we've succesfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+    const db = admin.firestore();
+    let today = new Date();
+    let datefield = today.toISOString().slice(0,10)
+
+    let poolRef = db.collection('Pools').doc(datefield);
+    let getDoc = poolRef.get().then(doc => {
+        if (!doc.exists) {
+            return res.send({result: "Does not exists"});
+        } else {
+            return res.send(doc.data());
+        }
+    })
+    .catch(err => {
+        console.log('Error getting document', err);
+        return res.send(err)
+    });
+});
+
+exports.getAllPool = functions.https.onRequest(async (req, res) => {
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const db = admin.firestore();
+    let poolRef = db.collection('Pools');
+    let payload = []
+
+    let allPools = poolRef.get().then(snapshot => {
+        snapshot.forEach(doc => {
+            payload.push(doc.data())
+            console.log(doc.id, '=>', doc.data());
+        });
+        return res.send(payload);
+    }).catch(err => {
+        console.log('Error getting documents', err);
+        return res.send(err);
+    });
+});
+
+exports.donate = functions.https.onRequest(async (req, res) => {
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    // Param: receiverId -> string
+    //        amount     -> number
+    const db = admin.firestore();
+
+    let today = new Date();
+    let datefield = today.toISOString().slice(0,10)
+
+    const receiverId = req.query.receiverId;
+    const donorId = req.query.donorId;
+    const amount = req.query.amount;
+
+    let poolRef = db.collection('Pools').doc(datefield);
+    let donorRef = db.collection('Donors').doc(donorId);
+
+    // Check if today's document exists
+
+    // Update Pool
+    await poolRef.update({ totalAmount: admin.firestore.FieldValue.increment(Number(amount)) });
+    
+    // Update Donar
+    await donorRef.update({ currentDayDonation: admin.firestore.FieldValue.increment(Number(amount)) });
+    await donorRef.update({
+        pastDonations: admin.firestore.FieldValue.arrayUnion({ amount: Number(amount), date: datefield})
+    });
+
+    res.json({result: "Success"});
+
+    // Update Receiver balance
+
 });
