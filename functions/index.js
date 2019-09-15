4e65b8f1551a724637caaba7433181377a8bf203
enochtangg <enoch.tang@hotmail.com>
2019-09-15 01:20:37 -0400

    post endpoints

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ exports.getCurrentPool = functions.https.onRequest(async (req, res) => {
     let datefield = today.toISOString().slice(0,10)
 
     let poolRef = db.collection('Pools').doc(datefield);
-    let getDoc = poolRef.get().then(doc => {
+    await poolRef.get().then(doc => {
         if (!doc.exists) {
             return res.send({result: "Does not exists"});
         } else {
@@ -36,10 +36,9 @@ exports.getAllPool = functions.https.onRequest(async (req, res) => {
     let poolRef = db.collection('Pools');
     let payload = []
 
-    let allPools = poolRef.get().then(snapshot => {
+    await poolRef.get().then(snapshot => {
         snapshot.forEach(doc => {
             payload.push(doc.data())
-            console.log(doc.id, '=>', doc.data());
         });
         return res.send(payload);
     }).catch(err => {
@@ -54,14 +53,33 @@ exports.donateToPool = functions.https.onRequest(async (req, res) => {
     let today = new Date();
     let datefield = today.toISOString().slice(0,10)
 
-    const receiverId = req.query.receiverId;
     const donorId = req.query.donorId;
     const amount = req.query.amount;
 
-    let poolRef = db.collection('Pools').doc(datefield);
+    let allPoolRef = db.collection('Pools');
     let donorRef = db.collection('Donors').doc(donorId);
 
-    // TODO: Check if today's document exists
+    // Add new date if not created
+    // await allPoolRef.get().then(snapshot => {
+    //     let hasDate = false;
+    //     snapshot.forEach(doc => {
+    //         if (datefield === doc.data().date) {
+    //             hasDate = true;
+    //         }
+    //     });
+    //     if (!hasDate) {
+    //         let data = {
+    //             date: datefield,
+    //             totalAmount: 0,
+    //         };
+    //         await db.collection('Pools').doc(datefield).set(data);
+    //     }
+    // }).catch(err => {
+    //     console.log('Error getting documents', err);
+    //     return res.send(err);
+    // });
+
+    let poolRef = db.collection('Pools').doc(datefield);
 
     // Update Pool
     await poolRef.update({ totalAmount: admin.firestore.FieldValue.increment(Number(amount)) });
@@ -72,14 +90,43 @@ exports.donateToPool = functions.https.onRequest(async (req, res) => {
         pastDonations: admin.firestore.FieldValue.arrayUnion({ amount: Number(amount), date: datefield})
     });
 
-    res.json({result: "Success"});
+    // Update Receiver balance
+    await db.collection("Receivers").get().then(snapshot => {
+        let numberOfReceivers = snapshot.size;
+        snapshot.forEach(async doc => {
+            let receiversRef = db.collection("Receivers").doc(doc.id);
+            let dividedAmount = Number(amount) / numberOfReceivers;
+            await receiversRef.update({ balance: admin.firestore.FieldValue.increment(Number(dividedAmount)) });
+        });
+        return res.json({result: "Success"});
+    });
+    return res.json({result: "An an error has occurred"});
+});
 
-    // TODO: Update Receiver balance
+exports.donateToIndividual = functions.https.onRequest(async (req, res) => {
+    const db = admin.firestore();
+
+    let today = new Date();
+    let datefield = today.toISOString().slice(0,10)
+    
+    const donorId = req.query.donorId;
+    const receiverId = req.query.receiverId;
+    const amount = req.query.amount;
+
+    let donorRef = db.collection('Donors').doc(donorId);
+    await donorRef.update({ currentDayDonation: admin.firestore.FieldValue.increment(Number(amount)) });
+    await donorRef.update({
+        pastDonations: admin.firestore.FieldValue.arrayUnion({ amount: Number(amount), date: datefield})
+    });
+
+    let receiverRef = db.collection('Receivers').doc(receiverId);
+    await receiverRef.update({ balance: admin.firestore.FieldValue.increment(Number(amount)) });
+
+    return res.json({result: "Success"});
 });
 
 exports.getDonorInfo = functions.https.onRequest(async (req, res) => {
     const db = admin.firestore();
-
     const donorId = req.query.donorId;
 
     let donorRef = db.collection('Donors').doc(donorId);
@@ -96,3 +143,20 @@ exports.getDonorInfo = functions.https.onRequest(async (req, res) => {
     });
 });
 
+exports.getReceiverInfo = functions.https.onRequest(async (req, res) => {
+    const db = admin.firestore();
+    const receiverId = req.query.receiverId;
+
+    let receiverRef = db.collection('Receivers').doc(receiverId);
+    await receiverRef.get().then(doc => {
+        if (!doc.exists) {
+            return res.send({result: "Does not exists"});
+        } else {
+            return res.send(doc.data());
+        }
+    })
+    .catch(err => {
+        console.log('Error getting document', err);
+        return res.send(err)
+    });
+});
